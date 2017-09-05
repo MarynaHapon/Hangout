1a3a1817ba6a94e31601649159786e247c317e3d
Maryna Hapon <marynahapon@gmail.com>
2017-09-05 16:15:25 +0300

    scrolling effect

---
+++ dev/static/js/main.js
@@ -0,0 +1,14 @@
+$(window).ready(function(){
+    var windowHeight = $(window).height();
+
+    $('.slide')
+    .height(windowHeight)
+    .scrollie({
+        scrollOffset: -50,
+        scrollingInView: function (elem) {
+            var backgroundColor = elem.data('background');
+            $('body').css('background-color', backgroundColor);
+        }
+    });
+
+});
