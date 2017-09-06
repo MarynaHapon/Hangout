77d8c62bb81e809992909ab73df9b2201a8ccca3
Maryna Hapon <marynahapon@gmail.com>
2017-09-07 00:30:04 +0300

    button
    - for IE

--- build/static/js/button.js
+++ build/static/js/button.js
@@ -2,16 +2,22 @@
 
     var buttons = document.getElementsByTagName('button');
 
-    var changeTextColor = function() {
-        if (this.id === 'btn-black') {
+    var changeTextColor = function(e) {
+        eventObject.preventDefault(e);
+
+        var elem = eventObject.getTarget(e);
+
+        if (elem.id === 'btn-black') {
             document.body.className = 'black';
-        } else if (this.id === 'btn-white') {
+        } else if (elem.id === 'btn-white') {
             document.body.className = 'white';
         }
     };
 
     for(var i = 0, len = buttons.length; i < len; i++) {
-        buttons[i].addEventListener('click', changeTextColor, false);
+
+        eventObject.addEvent(buttons[i], 'click', changeTextColor);
+
     }
 
 })();
