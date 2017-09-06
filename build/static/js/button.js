4c1b3da31cfca1d147765b581c427853fe930e0e
Maryna Hapon <marynahapon@gmail.com>
2017-09-06 08:10:04 +0300

    button
    - button.js with action listener
    - button-test.js (action 'click')

--- build/static/js/button.js
+++ build/static/js/button.js
@@ -1,16 +1,17 @@
-(function(){
+(function() {
+
     var buttons = document.getElementsByTagName('button');
 
+    var changeTextColor = function() {
+        if (this.id === 'btn-black') {
+            document.body.className = 'black';
+        } else if (this.id === 'btn-white') {
+            document.body.className = 'white';
+        }
+    };
+
     for(var i = 0, len = buttons.length; i < len; i++) {
-
-        buttons[i].onclick = function() {
-            if (this.id === 'btn-black') {
-                document.body.className = 'black';
-            } else if (this.id === 'btn-white') {
-                document.body.className = 'white';
-            }
-
-        };
+        buttons[i].addEventListener('click', changeTextColor, false);
     }
 
 })();
